3c9ddc8acb8dec3d05598f7c3c096ae95ca512c2
Manuel Mejia <manuel@buildinglink.com>
2019-09-29 20:45:51 -0400

    adding docs

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ echo Building. this may take a minute...
 npm run build
 
 # navigate into the build output directory
-cd dist
+cd docs
 
 # if you are deploying to a custom domain
  echo 'cubicalconsulting.com' > CNAME
 
-echo Deploying..
-git init
-git add -A
-git commit -m 'deploy'
+# echo Deploying..
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # deploy
-git push -f https://github.com/mejiamanuel57/cubical.git gh-pages
+# git push -f https://github.com/mejiamanuel57/cubical.git gh-pages
 
 cd -
