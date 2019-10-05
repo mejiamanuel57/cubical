7062509cb30bd73d07101a9e5b8b913305ba0bcb
Manuel Mejia <manuel@buildinglink.com>
2019-10-05 16:42:16 -0400

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,26 @@
-module.exports = { publicPath: process.env.BASE_URL }
-// const path = require('path')
-// const PrerenderSPAPlugin = require('prerender-spa-plugin')
-// const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
 
-// module.exports = {
-//   configureWebpack: (config) => {
-//     if (process.env.NODE_ENV !== 'production') return
+module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') return
 
-//     return {
-//       plugins: [
-//         new PrerenderSPAPlugin({
-//           staticDir: path.join(__dirname, 'docs'),
-//           routes: ['/', '/services', '/contact'],
-//           minify: {
-//             collapseBooleanAttributes: true,
-//             collapseWhitespace: true,
-//             decodeEntities: true,
-//             keepClosingSlash: true,
-//             sortAttributes: true
-//           },
-//           renderer: new Renderer({ headless: true })
-//         })
-//       ]
-//     }
-//   }
-// }
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'docs'),
+          routes: ['/', '/services', '/contact'],
+          minify: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            keepClosingSlash: true,
+            sortAttributes: true
+          },
+          renderer: new Renderer({ headless: true })
+        })
+      ]
+    }
+  }
+}
