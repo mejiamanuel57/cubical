8a6b2419a1671e3badd8531db92f8ca3f7811f89
Manuel Mejia <manuel@buildinglink.com>
2019-11-20 00:25:04 -0500

    adding the first case study

--- src/components/about/index.vue
+++ src/components/about/index.vue
@@ -1,23 +1,19 @@
 <template>
-  <v-col cols="12">
-    <v-container fluid>
-      <v-card>
-        <v-row no-gutters>
-          <v-col cols="12" md="7">
-            <v-container fluid>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-row no-gutters>
+            <v-col cols="12" md="7">
               <v-img
                 :src="require('@/assets/cubical-about-us.jpg')"
               >
-                <v-container class="text-center pa-5">
-                  <h1 class="display-1">
-                    Our goal is to help you achieve want you want in the best possible way. The more you achieve, the more we grow.
-                  </h1>
-                </v-container>
+                <h1 class="display-1 text-center pa-5">
+                  Our goal is to help you achieve your business goals in the best possible way. The more you achieve, the more we grow.
+                </h1>
               </v-img>
-            </v-container>
-          </v-col>
-          <v-col cols="12" md="5">
-            <v-container>
+            </v-col>
+            <v-col cols="12" md="5" class="pa-4">
               <h4 class="display-1 text-center mb-3">
                 Why Cubical?
               </h4>
@@ -33,10 +29,102 @@
                 <li>Our payment plans are designed around milestones achieved, not crazy hourly rates</li>
                 <li>We provide excellent personalized customer service</li>
               </ul>
-            </v-container>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-container>
-  </v-col>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-row>
+            <v-col cols="12">
+              <h4 class="display-1 text-center mb-3">
+                Case Studies
+              </h4>
+            </v-col>
+            <v-col cols="12">
+              <v-card>
+                <v-row>
+                  <v-col cols="12" md="7">
+                    <v-img
+                      :src="require('@/assets/cubical-success.jpg')"
+                    />
+                  </v-col>
+                  <v-col cols="12" md="5" class="pa-4">
+                    <h4>INVENTORY - THE SIMPLER, THE BETTER</h4>
+                    <p>
+                      Your inventory can either be your most strategic asset or your biggest nightmare. If
+                      your company is a retailer, wholesaler, and/or manufacturer, your sales depend on
+                      how well managed is your inventory. Having a lot of inventory is risky and
+                      expensive, while not having enough can handicap your growth.We helped a
+                      wholesaler overcome the tragedy of overstock, out of stocks, accounts payables,
+                      and constant complaints from clients.
+                    </p>
+                    <p>
+                      Seymeh, an importer and wholesaler of electrical supplies for
+                      construction, was struggling with constant complaints from the
+                      sales team. This company was growing exponentially year after year
+                      to the point of not meeting the demand. To make it worst, the
+                      accounts payables and the procurement teams were challenged
+                      with not enough cash and weak relationships with suppliers.
+                    </p>
+                    <p>
+                      The objective of this project was to reduce the amount of sales
+                      loses due to out of stock, and decrease the level of inventory kept of
+                      slow-moving items. Our approach was to work from the roots up;
+                      starting with a deep understanding of the demand, all the way to
+                      the liquidation of slow movers and the presentation of proposals to
+                      negotiate with suppliers. The actions taken were:
+                    </p>
+                    <ul>
+                      <li>
+                        Create of a forecasting model that fitted the demand
+                        behavior better than simple averages to decrease the errors
+                        on estimations. External parameters were researched and
+                        included into the model to make sure it was robust.
+                      </li>
+                      <li>
+                        Gather data of missed sales due to out of stock to integrate it
+                        into the forecasting model.
+                      </li>
+                      <li>
+                        Estimate the hidden cost associated with storage of all items.
+                      </li>
+                      <li>
+                        Construct a purchasing model to recommend order
+                        quantities based on the items’ profitability, lead time, and
+                        demand.
+                      </li>
+                      <li>
+                        Simplify the portfolio of products offered, taking out items
+                        that were not profitable enough, did not meet the
+                        requirements of clients, or could be easily replaced by others
+                        with better performance
+                      </li>
+                      <li>
+                        Reduce the number of suppliers per category of products to
+                        increase the volume of orders and therefore the bargaining
+                        power of the company
+                      </li>
+                      <li>
+                        Project the company’s cash flow considering all the changes
+                        previously described and make new payment agreements
+                        with suppliers to strengthen the position of the company
+                      </li>
+                    </ul>
+                    <p>
+                      After weeks of arduous work, we helped this company increase 11%
+                      profitability thanks to a simpler catalog of products and stronger
+                      alliances. We also collaborated with the reduce of their cash
+                      conversion cycle by 60%. All this, without dropping the sales
+                      number.
+                    </p>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
