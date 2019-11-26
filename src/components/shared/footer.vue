fc147effb939a152f16fd6ac079c0dc12af0cd91
Manuel Mejia <manuel@buildinglink.com>
2019-11-25 19:46:54 -0500

    addind new address

--- src/components/shared/footer.vue
+++ src/components/shared/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <v-footer color="accent" dark>
-    <v-container fluid grid-list-md text-center>
+    <v-container fluid text-center>
       <v-row no-gutters>
         <v-col cols="12">
           <h4 class="display-1 text-xs-center">
@@ -8,23 +8,23 @@
           </h4>
         </v-col>
         <v-col cols="12">
-          <v-row no-gutters>
-            <v-flex xs12 md2 offset-md3>
+          <v-row justify="center" align="center">
+            <v-col cols="12">
               <v-icon>mdi-phone</v-icon> 647-554-6883
-            </v-flex>
-            <v-flex xs12 md2>
+            </v-col>
+            <v-col cols="12">
               <v-icon>mdi-email</v-icon> info@cubicalconsulting.com
-            </v-flex>
-            <v-flex xs12 md2>
-              <v-icon>mdi-map-marker</v-icon> Toronto,ON
-            </v-flex>
+            </v-col>
+            <v-col cols="12">
+              <v-icon>mdi-map-marker</v-icon>
+              2300 Yonge Street, Suite 1600, Toronto, On (M4P 1E4)
+            </v-col>
           </v-row>
+        </v-col>
+        <v-col cols="12" class="mt-10 text-center">
+          &copy;  {{ new Date().getFullYear() }}  <strong>cubicalconsulting.com</strong>
         </v-col>
       </v-row>
     </v-container>
-
-    <v-col cols="12" class="mt-10 text-center">
-      &copy;  {{ new Date().getFullYear() }}  <strong>cubicalconsulting.com</strong>
-    </v-col>
   </v-footer>
 </template>
